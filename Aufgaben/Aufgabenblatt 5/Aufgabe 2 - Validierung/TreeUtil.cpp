688b19544e99fe44df92a242bc6f17ebbff7933d
Ashura <ashura@localhost.localdomain>
2018-11-22 16:26:10 +0100

    Revert refactor

--- Aufgaben/Aufgabenblatt 5/Aufgabe 2 - Validierung/TreeUtil.cpp
+++ Aufgaben/Aufgabenblatt 5/Aufgabe 2 - Validierung/TreeUtil.cpp
@@ -4,44 +4,46 @@
 #include "TreeUtil.h"
 
 bool TreeUtil::isBinarySearchTree(std::vector<int> values) {
-  return isBinarySearchTree(std::move(values), 0, INT_MIN, INT_MAX);
+    return isBinarySearchTree(std::move(values), 0, INT_MIN, INT_MAX);
 }
 
 bool TreeUtil::isBinarySearchTree(std::vector<int> values, int nodePos, int leftBound, int rightBound) {
-  int nodeLeft = getLeftNode(values, nodePos);
-  int nodeRight = getRightNode(values, nodePos);
+    int nodeLeft = getLeftNode(values, nodePos);
+    int nodeRight = getRightNode(values, nodePos);
+    bool valid = true;
 
-  if (nodeLeft != -1 && // Node Exists
-      (
-              values[nodeLeft] >= values[nodePos] || // Left Node is smaller
-              values[nodeLeft] <= leftBound ||
-              !isBinarySearchTree(values, nodeLeft, leftBound, values[nodePos])
-      )) {
-    return false;
+    if (nodeLeft != -1) {
+        if (values[nodeLeft] < values[nodePos] && values[nodeLeft] > leftBound) {
+            valid &= isBinarySearchTree(values, nodeLeft, leftBound, values[nodePos]);
+        } else {
+            valid = false;
+        }
+    }
+    if (nodeRight != -1) {
+        if (values[nodeRight] > values[nodePos] && values[nodeRight] < rightBound) {
+            valid &= isBinarySearchTree(values, nodeRight, values[nodePos], rightBound);
+        } else {
+            valid = false;
+        }
+    }
 
-  }
-  if (nodeRight != -1 && (values[nodeRight] <= values[nodePos] || values[nodeRight] >= rightBound ||
-                          !isBinarySearchTree(values, nodeRight, values[nodePos], rightBound))) {
-    return false;
-  }
-
-  return true;
+    return valid;
 }
 
 int TreeUtil::getRightNode(std::vector<int> values, int nodePos) {
-  int rightNode = nodePos * 2 + 2;
-  if (values.size() <= rightNode) {
-    return -1;
-  }
+    int rightNode = nodePos * 2 + 2;
+    if (values.size() <= rightNode) {
+        return -1;
+    }
 
-  return rightNode;
+    return rightNode;
 }
 
 int TreeUtil::getLeftNode(std::vector<int> values, int nodePos) {
-  int leftNode = nodePos * 2 + 1;
-  if (values.size() <= leftNode) {
-    return -1;
-  }
+    int leftNode = nodePos * 2 + 1;
+    if (values.size() <= leftNode) {
+        return -1;
+    }
 
-  return leftNode;
+    return leftNode;
 }
